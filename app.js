9e50709fe3d4d203b748906d6916a3e4c34009d6
PIYUSH-GOYAL1 <piyushgoyal11824@gmail.com>
2024-07-12 10:37:32 +0530

    second update initialised

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
+app.set("view engine" , "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname , "public")));
-app.set("view engine" , "ejs");
 
 app.get("/" , (req,res)=>{
     try{
         res.render("index");
     }catch(err){
-        res.render("error" , {msg : error});
+        res.render("error" , {msg : err});
     }
 });
 
